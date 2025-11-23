976991ca0331f70bcc603ecd9429742281f5706c
Konrad Painta <konradpainta@gmail.com>
2025-11-23 22:00:21 +0000

    Convert pipeline to stream

--- lib/agents/agent_pipeline.dart
+++ lib/agents/agent_pipeline.dart
@@ -2,41 +2,30 @@ import 'package:note_demo/agents/agent_utils.dart';
 import 'package:note_demo/agents/gpt_agent.dart';
 import 'package:note_demo/models/agent_responses/models.dart';
 
+typedef PipelineStatus<T> = ({int index, T object, bool finished});
+
 class AgentPipeline {
+  int pipeLength;
+  List<String> promptPipe;
+
   final role = AgentRole.pipeline;
-  final promptPipe = [
-    """<System Instructions>
-        You are the first step in a resource fetching and synthesising pipeline. 
-        Your job is to return up to 4 links for online content related to the provided content.
-        It can be blog posts, articles or youtube videos. 
 
-        Respond in a comma seperated list.
-        </System Instructions> """,
-    """
-      <System Instructions> You are the second step in a resource fetching and synthesising pipeline. 
-      Based on this list of resources, visit each one, then rank them in order of usefulness. make sure the links are valid.
-      </System Instructions>""",
-    """
-      <System Instructions> You are the final step in a resource fetching and synthesising pipeline.
-      The final step is to synthesise the list of evaluated resources.
-      Your output will be displayed to a student using an ai study companion app, under a helpful "Next Steps" section, 
-      so it must follow the following criteria:
+  AgentPipeline(this.pipeLength, {required this.promptPipe}) {
+    assert(
+      pipeLength == promptPipe.length,
+      "Pipelength must be equal to prompt number in pipe!",
+    );
+    promptPipe = promptPipe;
+  }
 
-      - Under 20 words
-      - Must include the link
-      - .md format
-     </System Instructions>
-    """,
-  ];
-
-  Future<String> fetch(String initial) async {
+  Stream<PipelineStatus<String>> fetch(String initial) async* {
     final agents = List.generate(
-      promptPipe.length,
+      pipeLength,
       (_) => GPTAgent<BaseResponse>(role: role),
     );
     var responseChain = [initial];
 
-    for (var i in Iterable.generate(agents.length)) {
+    for (var i in Iterable.generate(pipeLength)) {
       final agent = agents[i];
       final prompt = promptPipe[i];
 
@@ -44,10 +33,11 @@ class AgentPipeline {
         final next = await agent.fetch('$prompt ${responseChain.last}');
         responseChain.add(next.content);
         print("pipeline ${i + 1} success, returned: ${next.content}");
+        yield (index: i + 1, object: next.content, finished: false);
       } catch (e) {
         responseChain.add(e.toString());
       }
     }
-    return responseChain.last;
+    yield (index: 0, object: responseChain.last, finished: true);
   }
 }
--- lib/providers/external_research_provider.dart
+++ lib/providers/external_research_provider.dart
@@ -6,11 +6,11 @@ import 'package:note_demo/models/agent_responses/models.dart';
 import 'package:note_demo/providers/models/models.dart';
 import 'package:note_demo/providers/principle_agent_provider.dart';
 
-class ExternalResearchNotifier extends Notifier<String?> {
+class ExternalResearchNotifier extends Notifier<ExternalResearchState> {
   @override
-  String? build() {
+  ExternalResearchState build() {
     _subscribeToPrinciple();
-    return null;
+    return ExternalResearchState();
   }
 
   void _subscribeToPrinciple() {
@@ -27,22 +27,47 @@ class ExternalResearchNotifier extends Notifier<String?> {
 
   void _updateResearch() async {
     print("updating research");
+    state = state.copyWith(isLoading: true, pipeLevel: 0);
 
     final diff = ref.read(principleAgentProvider).diff?.additions ?? "";
 
-    final model = GPTAgent<ExternalResearchResponse>(
-      role: AgentRole.researcher,
-    );
+    final pipeline = AgentPipeline(3, promptPipe: kExternalResearchPromptPipe);
 
-    final pipeline = AgentPipeline();
-
-    final response = await pipeline.fetch(diff);
-
-    state = response;
+    await for (final result in pipeline.fetch(diff)) {
+      state = state.copyWith(pipeLevel: result.index);
+      if (result.finished) {
+        state = state.copyWith(isLoading: false, content: result.object);
+      }
+    }
   }
 }
 
 final externalResearchProvider =
-    NotifierProvider<ExternalResearchNotifier, String?>(
+    NotifierProvider<ExternalResearchNotifier, ExternalResearchState>(
       () => ExternalResearchNotifier(),
     );
+
+const kExternalResearchPromptPipe = [
+  """<System Instructions>
+        You are the first step in a resource fetching and synthesising pipeline. 
+        Your job is to return up to 4 links for online content related to the provided content.
+        It can be blog posts, articles or youtube videos. 
+
+        Respond in a comma seperated list.
+        </System Instructions> """,
+  """
+      <System Instructions> You are the second step in a resource fetching and synthesising pipeline. 
+      Based on this list of resources, visit each one, then rank them in order of usefulness. make sure the links are valid.
+      </System Instructions>""",
+  """
+      <System Instructions> You are the final step in a resource fetching and synthesising pipeline.
+      The final step is to synthesise the list of evaluated resources.
+      Your output will be displayed to a student using an ai study companion app, under a helpful "Next Steps" section, 
+      so it must follow the following criteria:
+
+      - Under 20 words
+      - Must include the link
+      - .md format
+     </System Instructions>
+    """,
+];
